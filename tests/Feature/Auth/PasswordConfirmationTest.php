76a852681cf9d3046a12b0bc798783ed45c526d6
Nolween Lopez <nolween.lopez@gmail.com>
2022-06-30 16:54:00 +0200

    PHP Unit - Test Confirmation Mot de passe To Do: Test Reset Mot de passe

--- tests/Feature/Auth/PasswordConfirmationTest.php
+++ tests/Feature/Auth/PasswordConfirmationTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Auth;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -10,9 +11,20 @@ class PasswordConfirmationTest extends TestCase
 {
     use RefreshDatabase;
 
+    // Initialisation d'un utilisateur
+    public function initialize_user()
+    {
+        // Création d'un rôle, nécessaire pour la création d'un utilisateur
+        Role::create(['name' => 'Administrateur']);
+        // Création d'un utilisateur
+        $user = User::create(['name' => 'Visiteur', 'email' => 'visiteur.test@test.com', 'password' => bcrypt('123456'), 'role_id' => 1, 'is_banned' => false, 'email_verified_at' => now()]);
+
+        return $user;
+    }
+
     public function test_confirm_password_screen_can_be_rendered()
     {
-        $user = User::factory()->create();
+        $user = $this->initialize_user();
 
         $response = $this->actingAs($user)->get('/confirm-password');
 
@@ -21,10 +33,10 @@ class PasswordConfirmationTest extends TestCase
 
     public function test_password_can_be_confirmed()
     {
-        $user = User::factory()->create();
+        $user = $this->initialize_user();
 
         $response = $this->actingAs($user)->post('/confirm-password', [
-            'password' => 'password',
+            'password' => '123456',
         ]);
 
         $response->assertRedirect();
@@ -33,7 +45,7 @@ class PasswordConfirmationTest extends TestCase
 
     public function test_password_is_not_confirmed_with_invalid_password()
     {
-        $user = User::factory()->create();
+        $user = $this->initialize_user();
 
         $response = $this->actingAs($user)->post('/confirm-password', [
             'password' => 'wrong-password',
